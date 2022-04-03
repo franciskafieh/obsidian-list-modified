1927383a4e996189ce4ee4f7d586268c0ec58bf2
Francis Kafieh <kafieh.francis@gmail.com>
2022-04-03 13:31:03 -0400

    prettier

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # Obsidian List Modified
+
 This plugin lists all modified files meeting certain criteria to a daily note.
 
 ![plugin demo gif](preview/obsidian-list-modified.gif)
 
 # Why?
+
 As of now, dataview queries cannot be "frozen" in time. There are hacky solutions using templater, but I wanted something really simple.
 
 # How?
+
 When a file is modified, the plugin checks if there is already a link to the modified file inside of your daily note. If so, it doesn't need to link it again. Otherwise, If this modified file meets the criteria, it appends a link to the daily note.
 
 # Criteria supported
+
 ## Blacklist tags
+
 Disables a modified note from linking if it contains a certain tag.
 
 These tags will be recognized if they are in the note's content (#tag anywhere in your note)
 
 or in its frontmatter in the following formats:
+
 ```yaml
 ---
 tags: ['#your', '#tags', '#here']
@@ -30,6 +36,7 @@ tags:
 ```
 
 # Instalation
+
 1. Open your vault's folder in your file explorer
 2. Make sure showing hidden files is enabled
 3. Open the `.obsidian` folder, then open the `plugins` folder.
@@ -41,4 +48,5 @@ tags:
 **NOTE:** You must have safe mode shut off to be able to install community plugins!
 
 # Usage
+
 The only things you need to configure are in the settings tab! After these are configured, the plugin should work automatically. Read the description of each setting carefully!
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
-import { CachedMetadata, Notice, Plugin, TFile, moment, TagCache, FrontMatterCache, TAbstractFile } from 'obsidian'
-import { ListModifiedSettings, DEFAULT_SETTINGS, ListModifiedSettingTab } from './settings'
+import {
+	CachedMetadata,
+	Notice,
+	Plugin,
+	TFile,
+	moment,
+	TagCache,
+	FrontMatterCache,
+	TAbstractFile,
+} from "obsidian";
+import {
+	ListModifiedSettings,
+	DEFAULT_SETTINGS,
+	ListModifiedSettingTab,
+} from "./settings";
 
 // const templates: readonly [string[], string[]] = [
 // 	['[[link]]', 'f'],
@@ -7,102 +20,123 @@ import { ListModifiedSettings, DEFAULT_SETTINGS, ListModifiedSettingTab } from '
 // ]
 
 export default class ListModified extends Plugin {
-	settings: ListModifiedSettings
-	blacklistedTags: string[]
-	currentFile: TFile
-	dailyFile: TFile
-	resolvedOutput: string
-	currentFileCache: CachedMetadata
-	dailyFileCache: CachedMetadata
-	dailyFileContent: string
+	settings: ListModifiedSettings;
+	blacklistedTags: string[];
+	currentFile: TFile;
+	dailyFile: TFile;
+	resolvedOutput: string;
+	currentFileCache: CachedMetadata;
+	dailyFileCache: CachedMetadata;
+	dailyFileContent: string;
 
 	async onload() {
-		await this.loadSettings()
+		await this.loadSettings();
 
-		this.registerEvent(this.app.vault.on('modify', this.onModify))
+		this.registerEvent(this.app.vault.on("modify", this.onModify));
 
-		this.addSettingTab(new ListModifiedSettingTab(this.app, this))
+		this.addSettingTab(new ListModifiedSettingTab(this.app, this));
 	}
 
 	onunload() {
-		this.app.vault.off('modify', this.onModify)
+		this.app.vault.off("modify", this.onModify);
 	}
 
 	onModify = async (file: TAbstractFile) => {
-		this.blacklistedTags = this.settings.tags.replace(/\s/g, '').split(',')
-		this.currentFile = file as TFile
+		this.blacklistedTags = this.settings.tags.replace(/\s/g, "").split(",");
+		this.currentFile = file as TFile;
 
-
-		const dailyNoteFormat: string = this.settings.dailyNoteFormat
+		const dailyNoteFormat: string = this.settings.dailyNoteFormat;
 		this.dailyFile = this.app.vault.getAbstractFileByPath(
-		moment().format(dailyNoteFormat) + '.md'
-		) as TFile
+			moment().format(dailyNoteFormat) + ".md"
+		) as TFile;
 
-
-		const outputFormat: string = this.settings.outputFormat
-		this.resolvedOutput = outputFormat.replace('[[link]]', `[[${this.currentFile.basename}]]`)
+		const outputFormat: string = this.settings.outputFormat;
+		this.resolvedOutput = outputFormat.replace(
+			"[[link]]",
+			`[[${this.currentFile.basename}]]`
+		);
 
 		if (!this.dailyFile) {
-			new Notice(`A daily file with format ${dailyNoteFormat} doesn't exist! Cannot append link`) 
-			return
+			new Notice(
+				`A daily file with format ${dailyNoteFormat} doesn't exist! Cannot append link`
+			);
+			return;
 		}
 
-		this.currentFileCache = this.app.metadataCache.getFileCache(this.currentFile)
-		this.dailyFileCache = this.app.metadataCache.getFileCache(this.dailyFile)
+		this.currentFileCache = this.app.metadataCache.getFileCache(
+			this.currentFile
+		);
+		this.dailyFileCache = this.app.metadataCache.getFileCache(
+			this.dailyFile
+		);
 
-		if (this.currentFile === this.dailyFile) return
+		if (this.currentFile === this.dailyFile) return;
 
-		if (this.fileIsLinked()) return
+		if (this.fileIsLinked()) return;
 
-		if (!this.fileMeetsTagRequirements()) return
+		if (!this.fileMeetsTagRequirements()) return;
 
-		this.dailyFileContent = await this.app.vault.read(this.dailyFile)
+		this.dailyFileContent = await this.app.vault.read(this.dailyFile);
 
-		await this.appendLink()
-	}
+		await this.appendLink();
+	};
 
 	async appendLink(): Promise<void> {
-		if (this.dailyFileContent.slice(-1) === '\n') {
-			await this.app.vault.modify(this.dailyFile, this.dailyFileContent + this.resolvedOutput + '\n')
+		if (this.dailyFileContent.slice(-1) === "\n") {
+			await this.app.vault.modify(
+				this.dailyFile,
+				this.dailyFileContent + this.resolvedOutput + "\n"
+			);
 		} else {
-			await this.app.vault.modify(this.dailyFile, this.dailyFileContent + '\n' + this.resolvedOutput)
+			await this.app.vault.modify(
+				this.dailyFile,
+				this.dailyFileContent + "\n" + this.resolvedOutput
+			);
 		}
 	}
 
 	fileIsLinked(): boolean {
-		if (!this.dailyFileCache.links) return false
-		return this.dailyFileCache.links.some(l => l.link === this.currentFile.basename)
+		if (!this.dailyFileCache.links) return false;
+		return this.dailyFileCache.links.some(
+			(l) => l.link === this.currentFile.basename
+		);
 	}
 
 	// broken
 	fileMeetsTagRequirements(): boolean {
 		for (const tag of this.blacklistedTags) {
-			if (this.tagMetadataContainsTag(tag) || 
-			this.frontmatterMetadataContainsTag(tag)) return false;
+			if (
+				this.tagMetadataContainsTag(tag) ||
+				this.frontmatterMetadataContainsTag(tag)
+			)
+				return false;
 		}
-		return true
+		return true;
 	}
 
 	tagMetadataContainsTag(tagToMatch: string): boolean {
-		const tagCache: TagCache[] = this.currentFileCache.tags
-		if (!tagCache) return false
+		const tagCache: TagCache[] = this.currentFileCache.tags;
+		if (!tagCache) return false;
 		// console.log('tag cache is ' + tagCache.forEach(console.log))
-		return tagCache.some(tag => tag.tag === tagToMatch)
+		return tagCache.some((tag) => tag.tag === tagToMatch);
 	}
 
 	frontmatterMetadataContainsTag(tagToMatch: string): boolean {
-		const frontmatterCache: FrontMatterCache = this.currentFileCache.frontmatter
-		if (!frontmatterCache) return false
-		return frontmatterCache.tags.some((tag: string) => tag === tagToMatch)
+		const frontmatterCache: FrontMatterCache =
+			this.currentFileCache.frontmatter;
+		if (!frontmatterCache) return false;
+		return frontmatterCache.tags.some((tag: string) => tag === tagToMatch);
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings)
+		await this.saveData(this.settings);
 	}
 }
-
-
