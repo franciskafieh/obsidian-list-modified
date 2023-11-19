56a140852466647cb82503549a1308b1238bbb18
Francis Kafieh <kafieh.francis@gmail.com>
2023-11-19 14:13:45 -0500

    added jest and initial example test

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	preset: "ts-jest",
+	moduleDirectories: ["node_modules", "src"],
+};
---
+++ tests/random.test.ts
@@ -0,0 +1,5 @@
+import testFn from "../src/utils/testFn";
+
+test("a", () => {
+	expect(testFn()).toBe("d");
+});
