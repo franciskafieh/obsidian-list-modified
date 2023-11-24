9b090d64a8ba438c9be9465892459a0a2b2e440e
Francis Kafieh <kafieh.francis@gmail.com>
2023-11-23 23:03:44 -0500

    added more interfaces

---
+++ src/logic/io/noteEditor.ts
@@ -0,0 +1,7 @@
+// export function writeToNote(... all the params, fileWriter: FileWriter (can be ObsidianFileWriter or MockFileWriter))
+
+import { IFile } from "src/model/IFile";
+
+export function writeToNote(file: IFile) {
+	// etc
+}
