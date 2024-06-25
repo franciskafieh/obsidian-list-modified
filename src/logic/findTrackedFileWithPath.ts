9cd3b505b847cddadc8258236e23583c9eef7251
Francis Kafieh <kafieh.francis@gmail.com>
2024-06-25 14:30:32 -0400

    updated deps and added obsidian-daily-notes-interface as a dep from github

---
+++ src/logic/findTrackedFileWithPath.ts
@@ -0,0 +1,8 @@
+import { ISettings } from "../interfaces/ISettings";
+
+export function findTrackedFileWithPath(
+	desiredPath: string,
+	settings: ISettings,
+) {
+	return settings.trackedFiles.find(({ path }) => path === desiredPath);
+}
---
+++ src/logic/isNewDay.ts
@@ -0,0 +1,19 @@
+import moment from "moment";
+import { ISettings } from "../interfaces/ISettings";
+
+export function isNewDay(settings: ISettings) {
+	const lastTrackedDate = moment(settings.lastTrackedDate);
+
+	let granularity = new Map([
+		["daily", "day"],
+		["weekly", "week"],
+		["monthly", "month"],
+	]).get(settings.logNoteType);
+
+	if (granularity) {
+		// @ts-ignore, will always be the right type
+		return !lastTrackedDate.isSame(moment(), granularity);
+	}
+
+	return false;
+}
