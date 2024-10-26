135355107fbe4a19a9cb6d3243720d5dea62de1d
Francis Kafieh <kafieh.francis@gmail.com>
2024-10-25 20:22:58 -0400

    remove accidental import

--- src/obsidian/implementations/context/ObsidianReplacementDictionary.ts
+++ src/obsidian/implementations/context/ObsidianReplacementDictionary.ts
@@ -4,7 +4,6 @@ import { TFile, moment } from "obsidian";
 import { getLogNote } from "../../log_note/logNote";
 import { getPlugin, getSettings } from "../../settings/settings";
 import { FileMetadataCacheProvider } from "../../../interfaces/context/FileMetadataCacheProvider";
-import { file } from "bun";
 
 export class ObsidianReplacementDictionary extends ReplacementDictionary {
 	replacements = [
